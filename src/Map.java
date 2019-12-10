c34b5959c61fd8fd331623e4b19a230004260b16
Tenson Cai <tensoncai@Yings-MacBook-Air-9.local>
2019-12-09 22:15:35 -0600

    removed unused imports

--- src/Map.java
+++ src/Map.java
@@ -1,9 +1,6 @@
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
 public class Map {
 	// current board configuration
